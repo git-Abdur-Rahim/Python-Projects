756dbcb3aa31830032a41301f64fd2d1976d9863
git-Abdur-Rahim <98699053+git-Abdur-Rahim@users.noreply.github.com>
2022-12-11 19:58:32 +0530

    Update Students Dashboard.py

--- Students Dashboard.py
+++ Students Dashboard.py
@@ -5,7 +5,7 @@ marks = [45, 78, 12, 14, 48, 43, 47, 98, 35, 80]
 
 def display_dash_board(student_names, marks):
     data_base = dict(zip(student_names,
-                         marks))  # https://stackoverflow.com/questions/57143342/how-do-you-find-percentile-for-the-values-of-a-dictionary-in-python/57143740#57143740
+                         marks)) 
     # sorting the dictionary data_base wrt the marks in Ascending order
     sorted_data_base = sorted(data_base.items(), key=lambda kv_pair: kv_pair[1])
     least5_students = sorted_data_base[:5]
@@ -14,13 +14,13 @@ def display_dash_board(student_names, marks):
     sorted_data_base1 = sorted(data_base.items(), key=lambda kv_pair: kv_pair[1], reverse=True)
     top5_students = sorted_data_base1[:5]
 
-    for k, v in sorted_data_base:  # https://stackoverflow.com/questions/57143342/how-do-you-find-percentile-for-the-values-of-a-dictionary-in-python
+    for k, v in sorted_data_base:  
         percentile25 = v > 25
         percentile75 = v < 75
         if (percentile25) and (percentile75):
             print(k, v)
     print()
-    for k, v in top5_students:  # https://stackoverflow.com/questions/57143342/how-do-you-find-percentile-for-the-values-of-a-dictionary-in-python
+    for k, v in top5_students:  
         print(k, v)
     print()
     for k, v in least5_students:
