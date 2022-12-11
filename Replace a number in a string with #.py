7ed05565177e9c9652cd2042d414dbd886c9c9c9
git-Abdur-Rahim <98699053+git-Abdur-Rahim@users.noreply.github.com>
2022-12-11 20:00:30 +0530

    Update Replace a number in a string with #.py

--- Replace a number in a string with #.py
+++ Replace a number in a string with #.py
@@ -15,10 +15,10 @@ def replaced_String(res4,String):
 
 def replace_digits():
     String = str(input())
-   # m = re.sub("\d", "#", String)  # https://stackoverflow.com/questions/57007469/is-there-a-regex-to-replace-the-digits-in-the-string-with-and-remove-all-the
+   # m = re.sub("\d", "#", String)  
     print("The original string is : " + str(String))
                   # /D  = /d = [0-9]
-    res4 = re.sub("[^0-9]","", String)  #  https://www.kite.com/python/answers/how-to-remove-all-non-numeric-characters-from-a-string-in-python
+    res4 = re.sub("[^0-9]","", String) 
    # print("The resultant string : ",(re.sub("[0-9]","#",res4)))
 
     replaced_String(res4,String)
